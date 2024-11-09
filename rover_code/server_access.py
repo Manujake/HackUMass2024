59e8a4dc31fa6048ef2365fc82f2ca970c74323c
Ken Su <kensu324@gmail.com>
2024-11-09 10:28:22 -0500

    functioning server

--- rover_code/server_access.py
+++ rover_code/server_access.py
@@ -1,8 +1,8 @@
 from flask import Flask, request
-# from flask_cors import CORS
+from flask_cors import CORS
 
 app = Flask(__name__)
-# CORS(app)  # Enable CORS to handle cross-origin requests from AWS S3
+CORS(app)  # Enable CORS to handle cross-origin requests from AWS S3
 
 @app.route('/move', methods=['POST'])
 def move():
